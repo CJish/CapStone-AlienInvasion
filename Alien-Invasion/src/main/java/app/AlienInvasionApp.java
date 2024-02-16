d2029832a35b6d85ff15b065214b5721bb927a60
CJish <christopheru82@gmail.com>
2024-02-15 21:50:51 -0800

    Button is present in the game window, AB#423

--- Alien-Invasion/src/main/java/app/AlienInvasionApp.java
+++ Alien-Invasion/src/main/java/app/AlienInvasionApp.java
@@ -1,34 +1,72 @@
 package app;
 
+import Screens.Screen;
+import Screens.StartScreen;
 import client.*;
 import gameEngines.JsonWriter;
 import models.Player;
 import gameEngines.TextParser;
-import utils.UtilFunctions;
+import utils.asciiPanels.AsciiPanel;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Scanner;
 
-public class AlienInvasionApp {
+public class AlienInvasionApp extends JFrame implements KeyListener {
+
+    private AsciiPanel terminal;
+    private Screen screen;
+
+    private final CharacterStatus characterStatus = new CharacterStatus();
     private final Player player = new Player();
     private boolean isGame = true;
 
     public void run() {
         initialize();
-        UtilFunctions.clear();
+        CLS.clear();
         Scanner scan = new Scanner(System.in);
         TitleScreen.displayAsciiArt("./static/title.txt");
 
         if (NewGame.gameStart()) {
-            UtilFunctions.clear();
-            while (isGame()) {
+            CLS.clear();
+            while (isGame) {
                 AlienInvasionIntro.clearConsolePause(1000);
-                UtilFunctions.displayCharacterStatus(player);
+                characterStatus.displayCharacterStatus(player);
                 System.out.println("What's your next move Commander?");
                 System.out.print("> ");
                 String userInput = scan.nextLine().trim();
                 TextParser.textParser(userInput, player);
             }
         }
+    }
+
+    public static void createAndShowGUI() {
+        JFrame jFrame = new JFrame(); // creating an instance of JFrame
+        JButton jButton = new JButton("Alien Invasion"); // creating a button instance
+        jButton.setBounds(230, 100, 200, 40); // defining the button x,y, width, height
+
+        jFrame.add(jButton);
+        jFrame.setSize(800, 400);
+        jFrame.setLayout(null); // we could use a layout manager, null for now
+        jFrame.setVisible(true); // invisible by default
+    }
+
+//    public AlienInvasionApp() {
+//        super();
+//        terminal = new AsciiPanel();
+//        add(terminal);
+//        pack();
+//        screen = new StartScreen();
+//        addKeyListener(this);
+//        repaint();
+//    }
+
+    public void repaint() {
+        terminal.clear();
+        screen.displayOutput(terminal);
+        super.repaint();
     }
 
     public void initialize() {
@@ -39,6 +77,22 @@ public class AlienInvasionApp {
         JsonWriter.resetLocationsJSON();
     }
 
+    public void buttonClicked(KeyEvent k) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        screen = screen.respondToUserInput(e);
+        repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
     public boolean isGame() {
         return isGame;
     }
@@ -46,4 +100,5 @@ public class AlienInvasionApp {
     public void setGame(boolean game) {
         isGame = game;
     }
+
 }
