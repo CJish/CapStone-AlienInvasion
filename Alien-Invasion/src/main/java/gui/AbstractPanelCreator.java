64d98b56964e1ad4462a013e12f2e4208c7eabf0
SSPENELOPE <typoepping@gmail.com>
2024-02-23 15:06:40 -0500

    removed unecessary code, AB#1002

--- Alien-Invasion/src/main/java/gui/AbstractPanelCreator.java
+++ Alien-Invasion/src/main/java/gui/AbstractPanelCreator.java
@@ -10,8 +10,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public abstract class AbstractPanelCreator extends JPanel implements KeyListener {
-    public final int WINDOW_WIDTH = 800; // Change this to your desired window width
-    public final int WINDOW_HEIGHT = 600;
     private final String jLabelString;// = "Messages";
 
     public AbstractPanelCreator(String thisLabel) {
