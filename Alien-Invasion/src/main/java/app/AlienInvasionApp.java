2213b0bc8a103ccb229a1d0e912dd7f5fdfdaed4
GreenSpaceman22 <titus-kovalik@outlook.com>
2024-02-26 07:24:30 -0800

    added end game check into the game while loop in apps run() method. AB#995

--- Alien-Invasion/src/main/java/app/AlienInvasionApp.java
+++ Alien-Invasion/src/main/java/app/AlienInvasionApp.java
@@ -2,12 +2,16 @@ package app;
 
 import client.*;
 import gameEngines.JsonWriter;
+import gui.EndGameDisplay;
 import gui.MainDisplay;
 import gui.components.MessagesPanel;
 import models.Player;
 import gameEngines.TextParser;
+import utils.DisplayMethodsGUI;
 import utils.EndGameCriteria;
 import utils.UtilFunctions;
+
+import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.Scanner;
@@ -39,7 +43,6 @@ public class AlienInvasionApp  {
                 TextParser.textParser(userInput, player);
                 if (EndGameCriteria.isDroppedGas()) {
                     setGame(false);
-
                 }
             }
         }
